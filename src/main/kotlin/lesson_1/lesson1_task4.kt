54000477c3abd15cf1bcc53419a97fd6facb6944
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-10 10:35:39 +0300

    KS-1-4 work with variables

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val length = 40_868_600_000L
+    val age : Byte = 27
+    val partOfDay = 0.075f
+    val seconds : Short = 6480
+    val partOfYear = 2.0547945205479453E-4
+    val apogee = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
