59107a2a500e04694ac14d6bee61f6eaa2244fc6
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-10 10:30:48 +0300

    KS-1-2 work with variables

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders = 75
+    val feedbackOfOrders = "It's cool Order"
+    var numberOfPersonnels = 2000
+
+    println(numberOfOrders)
+    println(feedbackOfOrders)
+
+    --numberOfPersonnels
+    println(numberOfPersonnels)
+}
