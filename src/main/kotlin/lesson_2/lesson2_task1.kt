5bcb5d9afe7b7fbd7cc8e5eaf20f545887f87f73
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-15 09:24:15 +0300

    KS-2-5 fix format println

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -5,5 +5,5 @@ fun main() {
     val englishGrade = listOf(3.0, 4.0, 3.0, 5.0)
 
     val averageScore = englishGrade.sum() / numberOfStudents
-    println(averageScore)
+    println("%.2f".format(averageScore))
 }
