4248ee9c5126afa2992c01edbe285ae50397bdd4
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-16 09:31:06 +0300

    KS-1-5 fix format println

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -5,11 +5,10 @@ const val ONE_HOURS_IN_SECONDS = 3600
 
 fun main() {
 
-    val zero = 0
     val seconds = 6480
     val hours = seconds / ONE_HOURS_IN_SECONDS
     var remainingSeconds = seconds - hours * ONE_HOURS_IN_SECONDS
     val remainingMinutes = remainingSeconds / ONE_MINUTES_IN_SECONDS
     remainingSeconds -= remainingMinutes * ONE_MINUTES_IN_SECONDS
-    println("%d$hours:$remainingMinutes:%d$remainingSeconds".format(zero, zero))
+    println("%02d:%02d:%02d".format(hours,remainingMinutes,remainingSeconds))
 }
