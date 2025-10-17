82f05eac678efca0e8a9974f773027957cfd1ee6
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-17 16:18:06 +0300

    KS-2-5 fix variable totalPercent

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -7,7 +7,7 @@ fun main() {
     val startMoney = 70_000
     val percent = 16.7
     val depositPeriod = 20
-    val  totalPercent = 100
+    val totalPercent = 100
 
     val finishMoney = startMoney * (1 + percent / totalPercent).pow(depositPeriod)
 
