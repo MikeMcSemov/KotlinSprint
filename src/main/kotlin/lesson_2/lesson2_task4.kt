d2c9fb4b7e62834f23cebcd5261df962924a34a4
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-17 13:52:01 +0300

    KS-2-4 add variable totalPercent

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -5,7 +5,8 @@ fun main() {
     val buffs = 20
     val crystalOre = 7
     val ironOre = 11
+    val totalPercent = 100
 
-    println("Buffs with crystal ore: ${(crystalOre * buffs) / 100}")
-    println("Buffs with iron ore: ${(ironOre * buffs) / 100}")
+    println("Buffs with crystal ore: ${(crystalOre * buffs) / totalPercent}")
+    println("Buffs with iron ore: ${(ironOre * buffs) / totalPercent}")
 }
