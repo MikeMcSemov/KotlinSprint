ce0ec88530e105559f88a7f904dd7985e4bd9d0c
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-17 13:11:37 +0300

    KS-2-3 fix format println

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_2
 
+const val MINUTES_IN_HOUR = 60
+
 fun main() {
     val travelTime = 457
     val trainStartHour = 9
     val trainStartMinutes = 39
 
-    val startTimeMinutes = trainStartHour * 60 + trainStartMinutes
+    val startTimeMinutes = trainStartHour * MINUTES_IN_HOUR + trainStartMinutes
     val endTimeMinutes = startTimeMinutes + travelTime
 
-    println("${endTimeMinutes / 60}:${endTimeMinutes % 60}")
+    println("%02d:%02d".format((endTimeMinutes / MINUTES_IN_HOUR), (endTimeMinutes % MINUTES_IN_HOUR)))
 }
