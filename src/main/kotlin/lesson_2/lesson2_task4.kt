10201d8cddc9d15258b01e0f5a26e2f62fc249eb
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-17 13:23:19 +0300

    KS-2-4 fix

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,11 +1,11 @@
 package org.example.lesson_2
 
-fun  main() {
+fun main() {
 
-    val buffs = 0.2
+    val buffs = 20
     val crystalOre = 7
     val ironOre = 11
 
-    println("Buffs with crystal ore: ${(crystalOre*buffs).toInt()}")
-    println("Buffs with iron ore: ${(ironOre*buffs).toInt()}")
+    println("Buffs with crystal ore: ${(crystalOre * buffs) / 100}")
+    println("Buffs with iron ore: ${(ironOre * buffs) / 100}")
 }
