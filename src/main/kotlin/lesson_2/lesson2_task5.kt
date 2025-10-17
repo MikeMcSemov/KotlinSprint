3d5caafc4eb6413f969c05df779e1ab608d9fa84
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-17 13:44:55 +0300

    KS-2-5 fix println and add variable totalPercent

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,12 +2,14 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
-fun  main() {
+fun main() {
 
     val startMoney = 70_000
     val percent = 16.7
     val depositPeriod = 20
+    val  totalPercent = 100
 
-    val finishMoney = startMoney * (1 + percent / 100).pow(depositPeriod)
-    println(String.format("%.3f", finishMoney))
+    val finishMoney = startMoney * (1 + percent / totalPercent).pow(depositPeriod)
+
+    println("%.3f".format(finishMoney))
 }
