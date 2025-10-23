3c9f4c373f82b552facd629d8689b455166b81d1
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-23 17:24:36 +0300

    KS-5-4 working with if

---
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -0,0 +1,24 @@
+package org.example.lesson_5
+
+const val LOGIN_USER_IN_BASE = "Zaphod"
+const val PASSWORD_USER_IN_BASE = "PanGalactic"
+
+fun main() {
+
+    println("Приветствую вас на борту корабля \"Heart of Gold\"")
+
+    println("Введи, пожалуйста, свое имя пользователя, чтобы мы могли приступить к процессу входа.:")
+    val userName = readln()
+
+    if (userName == LOGIN_USER_IN_BASE) {
+        println("Введи, пожалуйста свой пароль:")
+        val userPassword = readln()
+        if (userPassword == PASSWORD_USER_IN_BASE) {
+            println("Пользователь \"$LOGIN_USER_IN_BASE\", вам разрешено входить на борт корабля \"Heart of Gold\".")
+        } else {
+            println("Неправильный пароль")
+        }
+    } else {
+        println("Такого пользователя нет. Необходимо зарегистрироваться")
+    }
+}
