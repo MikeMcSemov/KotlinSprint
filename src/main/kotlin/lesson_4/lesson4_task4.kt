ee116aa7016f5df83f5be11dea2ab25265cd65b9
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-10-24 10:55:28 +0300

    KS-4-4 fix variable and if

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,25 +1,20 @@
 package org.example.lesson_4
 
-const val EXERCISES_FOR_THE_MUSCLES_OF_THE_ARMS_AND_ABS = true
-const val EXERCISES_FOR_THE_MUSCLES_OF_THE_LEG_AND_BACK = !EXERCISES_FOR_THE_MUSCLES_OF_THE_ARMS_AND_ABS
-
 fun main() {
 
-    var trainingMuscles = EXERCISES_FOR_THE_MUSCLES_OF_THE_ARMS_AND_ABS
-
+    var exercisesOfArmsAndAbs = true
     val day = 5
 
-    if (day % 2 == 0) {
-        trainingMuscles = !trainingMuscles
-    } else {
+    if (day % 2 == 0)
+        exercisesOfArmsAndAbs = !exercisesOfArmsAndAbs
 
-    }
-
-    println("""
-        Упражнения для рук: ${trainingMuscles}
-        Упражнения для ног: ${!trainingMuscles}
-        Упражнения для спины: ${!trainingMuscles}
-        Упражнения для пресса: ${trainingMuscles}
-    """.trimIndent())
+    println(
+        """
+        Упражнения для рук: $exercisesOfArmsAndAbs
+        Упражнения для ног: ${!exercisesOfArmsAndAbs}
+        Упражнения для спины: ${!exercisesOfArmsAndAbs}
+        Упражнения для пресса: $exercisesOfArmsAndAbs
+    """.trimIndent()
+    )
 
 }
