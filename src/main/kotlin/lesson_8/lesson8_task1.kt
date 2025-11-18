f7183ef566cb4b90af453f73d26cb20e87172ccd
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-11-18 19:03:38 +0300

    KS-8-1 fix condition for

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -5,8 +5,8 @@ fun main() {
     val arrayOfShowInWeek = arrayOf(120, 540, 430, 385, 621, 809, 256)
     var sumShowInWeek = 0
 
-    for (i in 1..arrayOfShowInWeek.size) {
-        sumShowInWeek += arrayOfShowInWeek[i - 1]
+    for (i in arrayOfShowInWeek.indices) {
+        sumShowInWeek += arrayOfShowInWeek[i]
     }
 
     println(sumShowInWeek)
