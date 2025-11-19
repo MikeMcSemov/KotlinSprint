327daf5ac6e9ebca3a95591db6e2b27e3b11665c
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-11-19 17:58:51 +0300

    KS-7-5 fix variables range

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -3,14 +3,17 @@ package org.example.lesson_7
 fun main() {
 
     val listPassword: MutableList<Char> = mutableListOf()
-    val rangeSum = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    val rangeLowercaseLetters = 'a'..'z'
+    val rangeCapitalLetters = 'A'..'Z'
+    val rangeDigit = '0'..'9'
+    val rangeSum = rangeLowercaseLetters + rangeCapitalLetters + rangeDigit
 
     println("Введите длину кода, минимальное количество 6:")
     val sizeCode = readln().toInt()
 
-    listPassword.add(('a'..'z').random())
-    listPassword.add(('A'..'Z').random())
-    listPassword.add(('0'..'9').random())
+    listPassword.add(rangeLowercaseLetters.random())
+    listPassword.add(rangeCapitalLetters.random())
+    listPassword.add(rangeDigit.random())
 
     for (i in 4..sizeCode) {
         listPassword.add(rangeSum.random())
