323971fc75c55711d7cc96dd7ccb37fc9392a535
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-11-22 14:43:35 +0300

    KS-8-5 fix remarks

--- src/main/kotlin/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -1,20 +1,14 @@
 package org.example.lesson_8
 
 fun main() {
-
     println("Количество планируемых ингредиентов:")
     val numberOfIngredients = readln().toInt()
-
-
-    val arrayIngridients = Array(numberOfIngredients) {""}
+    val arrayIngridients = Array(numberOfIngredients) { "" }
 
     for (i in arrayIngridients.indices) {
-
         println("Введите ингредиент №${i + 1}")
         arrayIngridients[i] = readln()
-
     }
 
     println(arrayIngridients.joinToString())
-
 }
