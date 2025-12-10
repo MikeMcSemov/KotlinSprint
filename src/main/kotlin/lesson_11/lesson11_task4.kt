7dbad522b3febe2d520e72a72c77dcf8be563916
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2025-12-10 21:43:45 +0300

    KS-11-4 fix variable in class Recipe and class Ingredient

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -11,7 +11,6 @@ class Recipe(   //сущность рецепт
     val nameRecipe: String, //имя рецепта
     var isFavorite: Boolean = false,    //переменная, для добавления/удаления из избранного
     val ingredientList: List<Ingredient>, //список ингредиентов
-    val titleRecipePage: CategoryRecipe,    //заголовок страницы, берем из поля nameCategory класса CategoryRecipe
 )
 
 class Ingredient(   //сущность ингредиент
@@ -19,5 +18,4 @@ class Ingredient(   //сущность ингредиент
     var numberServings: Int = 1,    //количество порций, по умолчанию считаем для 1
     var numberIngredients: Int, //количество ингредиентов, на скринах указаны целые числа
     val unitMeasurement: String,    //единица измерения ингредиента
-    val titleIngredientPage: Recipe,    //заголовок страницы, берем из поля nameRecipe класса Recipe
 )
