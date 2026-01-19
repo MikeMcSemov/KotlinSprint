33134cedb52f3ee1bd7bc4546e3392bba1735e27
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2026-01-19 16:21:38 +0300

    KS-11-5 work with Class

---
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -0,0 +1,60 @@
+package org.example.lesson_11
+
+fun main() {
+
+    val forum = Forum()
+
+    val userFirst = forum.createNewUser("U1")
+    val userSecond = forum.createNewUser("U2")
+
+    forum.createNewMessage(0)
+    forum.createNewMessage(1)
+    forum.createNewMessage(0)
+    forum.createNewMessage(1)
+
+    forum.printThread()
+}
+
+
+class Forum(
+    val userName: String = "",
+    var userId: Int = 0,
+    var authorId: Int = 0,
+    val message: String = "",
+    val mutableListUserForum: MutableList<UserBuilder> = mutableListOf(),
+    val mutableListMessageForum: MutableList<MessageBuilder> = mutableListOf(),
+) {
+
+    fun createNewUser(userName: String) : UserBuilder {
+        mutableListUserForum.add(UserBuilder(userName = userName, userId = mutableListUserForum.size))
+        return mutableListUserForum[mutableListUserForum.lastIndex]
+    }
+
+    fun createNewMessage(userId: Int)  {
+        if (userId in mutableListUserForum.indices) {
+            println("Введите сообщение для ${mutableListUserForum[userId].userName}")
+            mutableListMessageForum.add(MessageBuilder(authorId = userId, readln()))
+        }
+    }
+
+    fun printThread() {
+        val mutableListMessageForumFilter = mutableListMessageForum.sortedBy { it -> it.message }
+        mutableListMessageForumFilter.forEach { println("Author: ${it.authorId} Message: ${it.message}") }
+    }
+
+    data class UserBuilder(
+        var userName: String = "",
+        var userId: Int = 0,
+    ) {
+        fun userName(userName: String) = apply { this.userName = userName }
+        fun userId(userId: Int) = apply { this.userId = userId }
+    }
+
+    data class MessageBuilder(
+        var authorId: Int = 0,
+        var message: String = "",
+    ) {
+        fun message(message: String) = apply { this.message = message }
+        fun authorId(authorId: Int) = apply { this.authorId = authorId }
+    }
+}
