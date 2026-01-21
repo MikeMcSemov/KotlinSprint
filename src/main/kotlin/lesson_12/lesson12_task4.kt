9208d669e44da578ea02ee6ea3110a6ce47ed999
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2026-01-21 15:06:25 +0300

    KS-12-4 work with constructor

---
+++ src/main/kotlin/lesson_12/lesson12_task4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_12
+
+fun main(){
+    val weatherOneDayNewNewNew = WeatherOneDayNewNewNew(300, 290)
+}
+
+class WeatherOneDayNewNewNew(temperatureDay: Int, temperatureNight: Int,) {
+
+    var precipitationPerDay: Boolean = false
+
+    init {
+        println("Day ${(temperatureDay - 273.15).toInt()} Night ${(temperatureNight - 273.15).toInt()} Precipitation $precipitationPerDay")
+    }
+}
