49a4a8793cbb0a4ed098dd39e38a7c7e78c9a086
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2026-01-23 14:48:47 +0300

    KS-6-2

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_6
 
-    const val COEF_FROM_MSEC_IN_SEC = 1000
+    const val COEF_FROM_MSEC_IN_SEC1 = 1000
 
 fun main() {
 
     println("Введите количество секунд, которые нужно засечь")
-    val numberOfSeconds = readln().toLong() * COEF_FROM_MSEC_IN_SEC
+    val numberOfSeconds = readln().toLong() * COEF_FROM_MSEC_IN_SEC1
 
     Thread.sleep(numberOfSeconds)
 
-    println("Прошло ${numberOfSeconds / COEF_FROM_MSEC_IN_SEC} секунд")
+    println("Прошло ${numberOfSeconds / COEF_FROM_MSEC_IN_SEC1} секунд")
 
 }
