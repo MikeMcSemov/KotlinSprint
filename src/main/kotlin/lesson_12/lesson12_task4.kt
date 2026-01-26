b34535de6f976c2fdf3d0337a1ca75f0f35e313c
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2026-01-26 13:08:47 +0300

    KS-12-4 fix variables class WeatherDay

--- src/main/kotlin/lesson_12/lesson12_task4.kt
+++ src/main/kotlin/lesson_12/lesson12_task4.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_12
 
 fun main() {
-    val weatherOneDayNewNewNew = WeatherOneDayNewNewNew(300, 290)
+    val weatherDay = WeatherDay(300, 290)
 }
 
-class WeatherOneDayNewNewNew(temperatureDay: Int, temperatureNight: Int) {
-
-    var precipitationPerDay: Boolean = false
+class WeatherDay(
+    val temperatureDay: Int,
+    val temperatureNight: Int,
+) {
+    val precipitationPerDay: Boolean = false
 
     init {
         println("Day ${(temperatureDay - 273.15).toInt()} Night ${(temperatureNight - 273.15).toInt()} Precipitation $precipitationPerDay")
