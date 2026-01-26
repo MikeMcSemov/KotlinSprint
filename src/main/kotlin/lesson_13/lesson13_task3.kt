7490c5d19bfade52b5a05189249ab766462bce84
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2026-01-26 21:58:57 +0300

    KS-13-3 fix println

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -1,10 +1,18 @@
 package org.example.lesson_13
 
 fun main() {
-    val listPhoneDirectory = listOf <PhoneDirectoryV2> (PhoneDirectoryV2("Ivan", 89991234567, null), PhoneDirectoryV2("Alex", 84567891234, "null"),
-        PhoneDirectoryV2("Toha", 87456123541, null))
+    val listPhoneDirectory = listOf (
+        PhoneDirectoryV2("Ivan", 89991234567, null),
+        PhoneDirectoryV2("Alex", 84567891234, "null"),
+        PhoneDirectoryV2("Toha", 87456123541, null),
+        PhoneDirectoryV2("Jack", 87456123641, "Oil"),
+        PhoneDirectoryV2("Pepper", 87456123742, "Gaz"),
+    )
 
-    listPhoneDirectory.forEach { println("Имя: ${it.name}\nНомер телефона: ${it.phoneNumber}\nКомпания: ${it.company}\n") }
+    println("Список уникальных компаний:")
+    listPhoneDirectory.forEach {
+        if (it.company != null) {
+        println(it.company)} }
 }
 
 class PhoneDirectoryV2(
