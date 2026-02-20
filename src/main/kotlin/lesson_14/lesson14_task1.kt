61b4b4792cfb2220e9eb69021058861546927fc0
MikeMcSemov <maksimow.mikhail2015@yandex.ru>
2026-02-20 11:03:20 +0300

    KS-14-1 fix work with open class

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -3,45 +3,44 @@ package org.example.lesson_14
 fun main() {
     val liner = ShipLiner()
     liner.printShip()
-    liner.isChopIce()
+    liner.checkIceBreakingCapacity()
 
     println()
 
-    val cargo = CargoShip("Грузовой", 40, 70)
+    val cargo = CargoShip()
     cargo.printShip()
-    cargo.isChopIce()
+    cargo.checkIceBreakingCapacity()
 
     println()
 
-    val iceDrift = IceDrift("Ледоход", 30, 30)
+    val iceDrift = IceDrift()
     iceDrift.printShip()
-    iceDrift.isChopIce()
+    iceDrift.checkIceBreakingCapacity()
 }
 
 open class ShipLiner(
-    val nameShip: String = "Лайнер",
-    val speed: Int = 60,
-    val liftingCapacity: Int = 100,
-    val chopIce: Boolean = false,
+    open val nameShip: String = "Лайнер",
+    open val speed: Int = 60,
+    open val liftingCapacity: Int = 100,
+    open val chopIce: Boolean = false,
 ) {
     fun printShip() {
         println("Тип корабля: $nameShip, Скорость: $speed км/ч, Вместительность: $liftingCapacity человек")
     }
-
-    fun isChopIce() {
+    fun checkIceBreakingCapacity() {
         if (chopIce) {println("$nameShip может колоть лёд")}
         else println("$nameShip не может колоть лёд")
     }
 }
 
 class CargoShip(
-    nameShip: String,
-    speed: Int,
-    liftingCapacity: Int,
+    override val nameShip: String = "Грузовой",
+    override val speed: Int = 40,
+    override val liftingCapacity: Int = 70,
     ) : ShipLiner(nameShip, speed, liftingCapacity, false)
 
 class IceDrift(
-    nameShip: String,
-    speed: Int,
-    liftingCapacity: Int,
+    override val nameShip: String = "Ледоход",
+    override val speed: Int = 30,
+    override val liftingCapacity: Int = 30,
 ) : ShipLiner(nameShip, speed, liftingCapacity, true)
